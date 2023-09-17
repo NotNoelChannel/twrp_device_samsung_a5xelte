8f97f6ff3acb61276b7e479746c0abaa049ab77b
NotNoelChannel <notnoel12345678@gmail.com>
2023-09-17 16:16:42 +0200

    a5xelte: Changes

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,43 +1,31 @@
 # Platform
-BOARD_VENDOR := samsung
-TARGET_BOARD_PLATFORM := exynos5
-TARGET_SOC := exynos7580
-TARGET_BOOTLOADER_BOARD_NAME := universal7580
-TARGET_NO_BOOTLOADER := true
-TARGET_NO_RADIOIMAGE := true
-
-# Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 :=
-TARGET_CPU_VARIANT := cortex-a53
+TARGET_CPU_VARIANT:= generic
 TARGET_CPU_SMP := true
-
 TARGET_2ND_ARCH := arm
 TARGET_2ND_ARCH_VARIANT := armv7-a-neon
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
-TARGET_2ND_CPU_VARIANT := cortex-a53
-
-TARGET_INIT_VENDOR_LIB := libinit_a5xelte
+TARGET_2ND_CPU_VARIANT := generic
+TARGET_BOARD_PLATFORM := exynos7580
+TARGET_BOOTLOADER_BOARD_NAME := universal7580
 
 # Kernel
-TARGET_KERNEL_ARCH := arm64
-TARGET_KERNEL_CONFIG := lineageos_a5xelte_defconfig
-TARGET_KERNEL_HEADER_ARCH := arm64
-TARGET_KERNEL_SOURCE := kernel/samsung/exynos7580-common
-BOARD_KERNEL_IMAGE_NAME := Image
-
-# Image
+TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/prebuilt/zImage
+TARGET_PREBUILT_DTB := $(LOCAL_PATH)/prebuilt/dtb
+BOARD_KERNEL_CMDLINE :=	buildvariant=eng
 BOARD_KERNEL_BASE := 0x10000000
+BOARD_KERNEL_TAGS_OFFSET := 0x00000100
 BOARD_KERNEL_PAGESIZE := 2048
-BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --dt device/samsung/a5xelte/prebuilt/dt.img --board SRPOJ08A000KU
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --dt $(LOCAL_PATH)/prebuilt/dtb
+
 
 # File systems
 BOARD_HAS_LARGE_FILESYSTEM := true
 TARGET_USERIMAGES_USE_EXT4 := true
-TARGET_USERIMAGES_USE_F2FS := true
 BOARD_SUPPRESS_SECURE_ERASE := true
 
 # Partitions
@@ -50,15 +38,28 @@ BOARD_SYSTEMIMAGE_PARTITION_SIZE := 3145728000
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 12096372736
 BOARD_FLASH_BLOCK_SIZE := 131072
 
-# TWRP specific build flags
-TW_THEME := portrait_hdpi
-RECOVERY_SDCARD_ON_DATA := true
-BOARD_HAS_NO_REAL_SDCARD := true
-TARGET_RECOVERY_PIXEL_FORMAT := "ABGR_8888"
-TW_BRIGHTNESS_PATH := "/sys/devices/14800000.dsim/backlight/panel/brightness"
+# Bootloader
+TARGET_NO_BOOTLOADER := true
 TW_NO_REBOOT_BOOTLOADER := true
 TW_HAS_DOWNLOAD_MODE := true
-TW_INCLUDE_CRYPTO := true
-TW_EXCLUDE_SUPERSU := true
-TW_USE_NEW_MINADBD := true
+TW_EXCLUDE_NANO := true
+TW_EXCLUDE_BASH := true
+# Display & Graphics
+TW_THEME                         := portrait_hdpi
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+TARGET_RECOVERY_PIXEL_FORMAT     := "RGBA_8888"
+TW_BRIGHTNESS_PATH               := "/sys/devices/14800000.dsim/backlight/panel/brightness"
 
+# TWRP Recovery
+TW_EXCLUDE_SUPERSU          := false
+BOARD_SUPPRESS_SECURE_ERASE := true
+
+# SHRP Recovery
+SHRP_MAINTAINER := notnoelchannel
+SHRP_EXPRESS := true
+SHRP_LITE := true
+SHRP_INTERNAL := /sdcard
+SHRP_REC := /dev/block/platform/13540000.dwmmc0/by-name/RECOVERY
+SHRP_EXTERNAL := /external_sd
+SHRP_OTG := /usb_otg
+SHRP_DEVICE_CODE := a5xelte
--- omni_a5xelte.mk
+++ omni_a5xelte.mk
@@ -1,18 +1,13 @@
-# Release name
-PRODUCT_RELEASE_NAME := a5xelte
-
-# Inherit from the common Open Source product configuration
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
-
-# Inherit from our custom product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, build/target/product/embedded.mk)
 
-PRODUCT_PACKAGES += \
-	charger_res_images \
-	charger
+$(call inherit-product, build/target/product/full_base_telephony.mk)
+$(call inherit-product, build/target/product/languages_small.mk)
 
-## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := a5xelte
-PRODUCT_NAME := omni_a5xelte
-PRODUCT_BRAND := samsung
+PRODUCT_DEVICE       := a5xelte
+PRODUCT_NAME         := omni_a5xelte
+PRODUCT_BRAND        := samsung
+PRODUCT_MODEL        := SM-A510F
 PRODUCT_MANUFACTURER := samsung
+PRODUCT_RELEASE_NAME := Samsung Galaxy A5 (2016)
+TARGET_VENDOR        := samsung
